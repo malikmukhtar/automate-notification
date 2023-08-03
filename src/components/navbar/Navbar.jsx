3a24dee8bdd16234b07ddb8db7feec5e1297dbda
malikmukhtar <malikmukhtar63@gmail.com>
2023-08-03 19:53:19 +0100

    initail commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './navbar.css'
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+                <span className="logo">Auto App</span>
+                <div className="icons">
+                    <img src="" alt="" className="iconImg" />
+                    <div className="counter">
+                        2
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Navbar

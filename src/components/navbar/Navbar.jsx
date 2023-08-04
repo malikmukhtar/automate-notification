2f3f3249c36e5e4903b73c1e018783c60f568c43
malikmukhtar <malikmukhtar63@gmail.com>
2023-08-04 22:57:07 +0100

    navbar layout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,31 @@
-import './navbar.css'
+import "./navbar.css";
+// import Notification from "../../img/notification.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faMessage, faGear } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-    return(
-        <div className="navbar">
-                <span className="logo">Auto App</span>
-                <div className="icons">
-                    <img src="" alt="" className="iconImg" />
-                    <div className="counter">
-                        2
-                    </div>
-                </div>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <span className="logo">Auto App</span>
+      <div className="icons">
+      <div className="icon">
+        <FontAwesomeIcon icon={faBell} className="iconImg" />
+        {/* <img src={Notification} className="iconImg" alt="" /> */}
+        <div className="counter">2</div>
+      </div>
+      <div className="icon">
+        <FontAwesomeIcon icon={faMessage} className="iconImg" />
+        {/* <img src={Notification} className="iconImg" alt="" /> */}
+        <div className="counter">2</div>
+      </div>
+      <div className="icon">
+        <FontAwesomeIcon icon={faGear} className="iconImg" />
+        {/* <img src={Notification} className="iconImg" alt="" /> */}
+        <div className="counter">2</div>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
